face4b21cd78a776aa79749d14da722333866d13
zieeco <zieecosam@gmail.com>
2022-03-02 00:52:44 +0100

    Add redux folder in the scr and remove generated boilerplate

---
+++ src/redux/configureStore.js
@@ -0,0 +1,11 @@
+https://github.com/microverse-students/repeat-a-string-TimmyChan99/pull/2
+
+
+ // "eslintConfig": {
+  //   "extends": [
+  //     "react-app",
+  //     "react-app/jest"
+  //   ]
+  // },
+  
+  https://github.com/microverse-students/fizzbuzz-VSeray/tree/Feature
